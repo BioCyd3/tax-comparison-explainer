45341a10385faddca17ca226371a8b7ef6d18dfd
Jeremy Jackson <jjackson@smazington.com>
2025-03-10 12:54:30 -0400

    Trigger GitHub Actions deployment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+// ./src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+import App from './App';
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById('root')); // Use createRoot
 root.render(
   <React.StrictMode>
     <App />
